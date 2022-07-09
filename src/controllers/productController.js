ba77a86d369a82132121dbe2279c872b90886844
Pedro Henrique Silva <driverique@gmail.com>
2022-07-08 21:38:42 -0300

    fixing error with params: heroku

--- src/controllers/productController.js
+++ src/controllers/productController.js
@@ -1,6 +1,5 @@
 
 import { db } from "../dbStategy/mongo.js";
-import { ObjectId } from "mongodb";
 
 export async function getProduct(req, res) {
     const products = await db.collection("product").find().toArray();
@@ -8,9 +7,9 @@ export async function getProduct(req, res) {
 }
 
 export async function getProductDetails(req, res) {
-    const id = req.body.product;
+    const idProduct = req.body.product;
 
-    const productSelected = await db.collection("product").find({_id: new ObjectId(id)}).toArray();
+    const productSelected = await db.collection("product").find({_id: idProduct}).toArray();
 
-    res.status(200).send(productSelected);
+    res.status(200).send('Deu bom meu querido toma o array', productSelected);
 }
