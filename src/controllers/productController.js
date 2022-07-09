ffacc221661116f22177b3bb432797cecda09f15
Pedro Henrique Silva <driverique@gmail.com>
2022-07-08 22:26:36 -0300

    Failed: restorin to original way

--- src/controllers/productController.js
+++ src/controllers/productController.js
@@ -1,15 +1,7 @@
 
 import { db } from "../dbStategy/mongo.js";
 
-
 export async function getProduct(req, res) {
     const products = await db.collection("product").find().toArray();
     res.status(200).send(products);
-}
-
-export async function getProductDetails(req, res) {
-    const { id } = req.body;
-    const productSelected = await db.collection("product").findOne({ id: {$eq: id} }).toArray();
-
-    res.status(200).send(productSelected);
 }
