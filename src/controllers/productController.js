53517f758bc07c879350ceb8d1fd79f739434e80
Pedro Henrique Silva <driverique@gmail.com>
2022-07-08 22:24:08 -0300

    Add new way to search item selected 2

--- src/controllers/productController.js
+++ src/controllers/productController.js
@@ -9,7 +9,7 @@ export async function getProduct(req, res) {
 
 export async function getProductDetails(req, res) {
     const { id } = req.body;
-    const productSelected = await db.collection("product").find({ id: id}).toArray();
+    const productSelected = await db.collection("product").findOne({ id: {$eq: id} }).toArray();
 
     res.status(200).send(productSelected);
 }
